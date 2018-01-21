3119925ab2bbec9293fdf66e55a64730ec518e41
Brent  Ransom <sadumedusa@gmail.com>
2018-01-21 23:54:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 		 alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-		 puts alans_new_info
 
 
 end
@@ -129,5 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
